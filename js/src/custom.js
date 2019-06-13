3dd2875584eebc560dc06586bdcc2a54eee30b7c
LiYpoi <2488943185@qq.com>
2019-06-13 23:53:30 +0800

    Site updated: 2019-06-13 23:53:26

---
+++ js/src/custom.js
@@ -0,0 +1,8 @@
+/* �ֲ�����ͼƬ */
+$(function () {
+    $.backstretch([  
+          "/images/background/pic1.jpg",
+          "/images/background/pic2.jpg",
+          "/images/background/pic3.jpg"
+    ], { duration: 30000, fade: 1500 });  
+});
