de34104a3f19c6168c5934cddb19bf34fa759177
LiYpoi <2488943185@qq.com>
2019-06-14 00:43:49 +0800

    Site updated: 2019-06-14 00:43:46

--- js/src/custom.js
+++
@@ -1,8 +0,0 @@
-/* �ֲ�����ͼƬ */
-$(function () {
-    $.backstretch([  
-          "/images/background/pic1.jpg",
-          "/images/background/pic2.jpg",
-          "/images/background/pic3.jpg"
-    ], { duration: 30000, fade: 1500 });  
-});
